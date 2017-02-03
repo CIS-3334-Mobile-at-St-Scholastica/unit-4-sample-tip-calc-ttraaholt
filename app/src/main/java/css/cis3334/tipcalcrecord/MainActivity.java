2ef9b6da8edaddf71843503d46eb743568fa48ee
tgibbons-css <tgibbons@css.edu>
2017-02-03 14:42:54 -0600

    Changed to Radio Buttons.

--- app/src/main/java/css/cis3334/tipcalcrecord/MainActivity.java
+++ app/src/main/java/css/cis3334/tipcalcrecord/MainActivity.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // Declare needed varaiable for widgets
     EditText etBill;
-    EditText etNumPeople;
     TextView tvTotalTip;
     TextView tvTipPerPerson;
     CheckBox cbService;
+    RadioButton rb1, rb2, rb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +22,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etBill = (EditText) findViewById(R.id.editTextBill);
-        etNumPeople = (EditText) findViewById(R.id.editTextNumPeople);
         tvTotalTip = (TextView) findViewById(R.id.textViewTotalTip);
         tvTipPerPerson = (TextView) findViewById(R.id.textViewTipPerPerson);
         cbService = (CheckBox) findViewById(R.id.checkBoxService);
+        rb1 = (RadioButton) findViewById(R.id.radioButton1);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rb3 = (RadioButton) findViewById(R.id.radioButton3);
     }
 
 
     public void onClickButtonCalc(View view) {
         //This is a comment
         Double bill = Double.parseDouble( etBill.getText().toString() );
-        Double numPeople = Double.parseDouble( etNumPeople.getText().toString() );
+        int numPeople = 1;
+        if (rb1.isChecked()) {
+            numPeople = 1;
+        }
+        if (rb2.isChecked()) {
+            numPeople = 2;
+        }
+        if (rb3.isChecked()) {
+            numPeople = 3;
+        }
         Double totalTip;
         if (cbService.isChecked()) {
             totalTip = bill * 0.2;
